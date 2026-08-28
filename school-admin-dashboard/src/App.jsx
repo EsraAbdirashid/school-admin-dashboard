243ddcfabf1f4023f1041c3442ba80afa747c4f3
EsraAbdirashid <esraabdirashid294@gmail.com>
2026-08-28 20:09:17 +0300

    Done

---
+++ school-admin-dashboard/src/App.jsx
@@ -0,0 +1,17 @@
+import Sidebar from "./Components/Sidebar";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Sidebar />
+
+      <main className="main-content">
+        <h1>Welcome back, Admin 👋</h1>
+        <p>Here's what's happening in your school today.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
