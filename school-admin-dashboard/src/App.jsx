bee0a34ad96b4fd4fb2299e7178bf648ae5c19dc
EsraAbdirashid <esraabdirashid294@gmail.com>
2026-08-28 20:11:41 +0300

    Done

--- school-admin-dashboard/src/App.jsx
+++ school-admin-dashboard/src/App.jsx
@@ -1,15 +1,8 @@
-import Sidebar from "./Components/Sidebar";
-import "./App.css";
-
 function App() {
   return (
-    <div className="app">
-      <Sidebar />
-
-      <main className="main-content">
-        <h1>Welcome back, Admin 👋</h1>
-        <p>Here's what's happening in your school today.</p>
-      </main>
+    <div>
+      <h1>ESRA DIGITAL</h1>
+      <p>School Management System</p>
     </div>
   );
 }
